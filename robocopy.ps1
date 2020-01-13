e6d185aec96ca68a915c390dcecb0fb57dc8924a
unknown <matthew_thorne@yahoo.co.uk>
2020-01-13 19:01:15 +0000

    Robocopy backup script hardcoded directory, responsive date for log name

---
+++ robocopy.ps1
@@ -0,0 +1,11 @@
+﻿$logpath="U:\test"
+#Log path
+
+$stamp=Get-Date -format "yyyy-MM-dd HH-mm"
+#Create date time stamp
+
+$filename="$stamp.log"
+#Format yyyy-mm-dd HH:mm in log filename
+
+robocopy C:\Users\Matt\Documents\Downloads U:\test /mir /xo /log:"$logpath\$filename"
+#Run robocopy
